0d635729f911ca4353a1c50f5bfcd469ff7e5e61
Aditya Raj <akaushik2@gitaam.in>
2024-02-11 20:25:02 +0530

    ok

--- script.js
+++ script.js
@@ -11,23 +11,38 @@ function changeSlide() {
     imgs[n].style.display = 'block';
 }
 
-changeSlide();
-
-prev_btn.addEventListener('click', (e) => {
-    if (n > 0) {
-        n--;
-    } else {
-        n = imgs.length - 1;
-    }
-    changeSlide();
-});
-
-next_btn.addEventListener('click', (e) => {
+function nextSlide() {
     if (n < imgs.length - 1) {
         n++;
     } else {
         n = 0;
     }
     changeSlide();
-});
+}
 
+function prevSlide() {
+    if (n > 0) {
+        n--;
+    } else {
+        n = imgs.length - 1;
+    }
+    changeSlide();
+}
+
+changeSlide();
+
+prev_btn.addEventListener('click', prevSlide);
+
+next_btn.addEventListener('click', nextSlide);
+
+
+setInterval(nextSlide, 5000);
+
+
+const scrollContainer =document.querySelectorAll('.products');
+for(const item of scrollContainer){
+    item.addEventListener('wheel', (evt)=>{
+        evt.preventDefault();
+        item.scrollLeft += evt.deltaY;
+    })
+}
